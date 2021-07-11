859376894cabfa91e4858ccbd72e1fd858a30332
Shamil <sha.carela@gmail.com>
2021-07-11 07:28:58 -0400

    Update: header text

--- src/pages/index.js
+++ src/pages/index.js
@@ -133,6 +133,8 @@ const IndexPage = () => {
       <h1 style={headingStyles}>
         Congratulations
         <br />
+        Gatsby is the best!
+        <br />
         <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
         <span role="img" aria-label="Party popper emojis">
           🎉🎉🎉
